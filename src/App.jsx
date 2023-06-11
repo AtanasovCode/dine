1e57507f4d128e898d4f9e1b09af1de519859858
AtanasovCode <a_ljupce@yahoo.com>
2023-06-12 00:29:52 +0200

    created theme and set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
 
+//Routes Import
+import Homepage from "./routes/Homepage";
 
 function App() {
+
+  const theme = {
+    beaver: "#9e7f66",
+    codGray: "#111111",
+    mirage: "#17192b",
+    ebonyClay: "#242b37",
+    shuttleGray: "#5c6779",
+
+    font: "League Spartan"
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />
+      //errorElement: <Error />
+    },
+  ])
+
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <RouterProvider router={router} />
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
+
+
+const Container = styled.div``;
