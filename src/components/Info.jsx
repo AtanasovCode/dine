14fc88a72f85439c59adc0ae47f33d946349f21d
AtanasovCode <a_ljupce@yahoo.com>
2023-06-13 16:59:32 +0200

    Info section of website now fully responsive

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,53 +1,68 @@
 import styled from "styled-components";
+
+//Importing Images
 import location from '../assets/images/enjoyable-place-mobile.png';
+import locationTablet from '../assets/images/enjoyable-place-tablet.png';
+
 import food from '../assets/images/local-food-mobile.png';
+import foodTablet from '../assets/images/local-food-tablet.png';
 
 const Info = () => {
     return (
         <Container>
-            <Section>
+            <Section 
+                direction="row"
+                first={true}
+            >
                 <FirstImage
                     src={location}
                     alt="location image"
                 />
 
-                <Title>
-                    <TitleSplit>
-                        Enjoyable place
-                    </TitleSplit>
-                    <TitleSplit>
-                        for all the family
-                    </TitleSplit>
-                </Title>
+                <Heading>
+                    <Title>
+                        <TitleSplit>
+                            Enjoyable place
+                        </TitleSplit>
+                        <TitleSplit>
+                            for all the family
+                        </TitleSplit>
+                    </Title>
 
-                <Subtitle>
-                    Our relaxed surroundings make dining
-                    with us a great experience for everyone.
-                    We can even arrange a tour of the farm before your meal.
-                </Subtitle>
+                    <Subtitle>
+                        Our relaxed surroundings make dining
+                        with us a great experience for everyone.
+                        We can even arrange a tour of the farm before your meal.
+                    </Subtitle>
+                </Heading>
             </Section>
 
-            <Section>
+            <Section 
+                direction="row-reverse"
+                first={false}
+            >
                 <SecondImage
                     src={food}
                     alt="location image"
                 />
 
-                <Title>
-                    <TitleSplit>
-                        The most locally
-                    </TitleSplit>
-                    <TitleSplit>
-                        sourced food
-                    </TitleSplit>
-                </Title>
+                <Heading>
+                    <Title>
+                        <TitleSplit>
+                            The most locally
+                        </TitleSplit>
+                        <TitleSplit>
+                            sourced food
+                        </TitleSplit>
+                    </Title>
 
-                <Subtitle>
-                    All our ingredients come directly 
-                    from our farm or local fishery. 
-                    So you can be sure that you’re 
-                    eating the freshest, most sustainable food.
-                </Subtitle>
+                    <Subtitle>
+                        All our ingredients come directly
+                        from our farm or local fishery.
+                        So you can be sure that you’re
+                        eating the freshest, most sustainable food.
+                    </Subtitle>
+                </Heading>
             </Section>
         </Container>
     );
@@ -61,6 +76,10 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     padding: 0 8vw;
+
+    @media (min-width: 1024px) {
+        padding: 0 12vw;
+    }
 `;
 
 const Section = styled.div`
@@ -70,20 +89,78 @@ const Section = styled.div`
     justify-content: center;
     z-index: 5;
     margin-bottom: 120px;
+
+    @media (min-width: 1024px) {
+        flex-direction: ${props => props.direction};
+    }
+
+    @media (min-width: 1024px) {
+        width: 100%;
+        justify-content: space-between;
+
+        //Apply margin bottom to the first section
+        margin-bottom: 0;
+        margin-bottom: ${props => props.first && "120px"};
+    }
 `;
 
-const Image = styled.img`
+const Image = styled.div`
     margin-bottom: 40px;
+    background-size: contain;
+    background-position: center;
+    background-position: center;
+    aspect-ratio: 9/16;
+    width: 327px;
+    max-height: 400px;
+    
+    @media (min-width: 768px) {
+        aspect-ration: none;
+        width: 570px;
+        max-height: 360px;
+    }
+
+    @media (min-width: 1024px) {
+        width: 540px;
+        max-height: 720px;
+        margin-bottom: 0;
+    }
 `;
 
 const FirstImage = styled(Image)`
     margin-top: -20%;
+    background-image: url(${location});
+
+    @media (min-width: 768px) {
+        margin-top: -15%;
+        background-image: url(${locationTablet});
+    }
+
+    @media (min-width: 1024px) {
+        margin-top: -5%;
+    }
 `;
 
 const SecondImage = styled(Image)`
     margin-top: 0;
+    background-image: url(${food});
+
+    @media (min-width: 768px) {
+        background-image: url(${foodTablet});
+    }
 `;
 
+const Heading = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    @media (min-width: 1024px) {
+        margin: 0;
+        align-items: flex-start;
+    }
+
+`;
 
 const Title = styled.div`
     font-size: 32px;
@@ -91,6 +168,16 @@ const Title = styled.div`
     line-height: 40px;
     text-align: center;
     margin-bottom: 20px;
+
+    @media (min-width: 768px) {
+        font-size: 48px;
+        line-height: 48px;
+    }
+
+    @media (min-width: 1024px) {
+        text-align: left;
+        align-self: left;
+    }
 `;
 
 const TitleSplit = styled.div``;
@@ -100,4 +187,15 @@ const Subtitle = styled.div`
     font-weight: 400;
     line-height: 26px;
     text-align: center;
+    max-width: 327px;
+
+    @media (min-width: 768px) {
+        font-size: 20px;
+        line-height: 30px;
+        max-width: 457px;
+    }
+
+    @media (min-width: 1024px) {
+        max-width: 445px;
+    }
 `;
