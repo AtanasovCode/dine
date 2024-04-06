1ea2bd095c3994a99b0a659be6f31aa1e6d4da6e
AtanasovCode <a_ljupce@yahoo.com>
2024-04-06 13:19:47 +0200

    added font & hero background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,20 @@
+import backgroundMobile from '../assets/homepage/hero-bg-mobile.jpg';
+import logo from '../assets/logo.svg';
+
 const Hero = () => {
     return (
-        <div></div>
+        <div className="relative">
+            <img src={backgroundMobile} alt="image of a pizza used as background image to the hero section" />
+
+            <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center">
+                <img src={logo} alt="dine logo" />
+
+                <div>
+                    Exquisite dining
+                    since 1989
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,7 +1,9 @@
+import Hero from "../components/Hero";
+
 const Homepage = () => {
     return (
         <div>
-            <h1></h1>
+            <Hero />
         </div>
     );
 }
