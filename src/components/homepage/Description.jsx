444f36aaeb356f021644fc37a0f8ae0f1ec3c3fa
AtanasovCode <a_ljupce@yahoo.com>
2024-04-07 17:09:48 +0200

    improved responsive design for hero image changed the hero image to a <picture> tag with several source tags

--- src/components/homepage/Description.jsx
+++ src/components/homepage/Description.jsx
@@ -1,3 +1,5 @@
+
+
 const Description = () => {
     return (
         <div></div>
--- src/components/homepage/HeroImage.jsx
+++ src/components/homepage/HeroImage.jsx
@@ -5,28 +5,28 @@ import backgroundTablet2x from '../../assets/homepage/hero-bg-tablet@2x.jpg';
 import backgroundDesktop from '../../assets/homepage/hero-bg-desktop.jpg';
 import backgroundDesktop2x from '../../assets/homepage/hero-bg-desktop@2x.jpg';
 
+import test from '../../assets/booking/hero-bg-desktop@2x.jpg';
+
 const HeroImage = () => {
     return (
-        <>
-            {/*Mobile*/}
-            <img
-                srcSet={`${backgroundMobile} 1x, ${backgroundMobile2x} 2x`}
-                alt="image of a pizza used as background image to the hero section"
-                className="w-full sm:hidden"
-            />
+        <picture className="w-full">
             {/*Tablet*/}
-            <img
-                srcSet={`${backgroundTablet} 1x, ${backgroundTablet2x} 2x`}
-                alt="image of a pizza used as background image to the hero section"
-                className="hidden sm:block lg:hidden w-full"
+            <source
+                media='(max-width: 500px)'
+                srcSet={`${backgroundMobile} 1x, ${backgroundMobile2x} 2x`}
             />
             {/*Desktop*/}
+            <source
+                media='(max-width: 1100px)'
+                srcSet={`${backgroundTablet} 1x, ${backgroundTablet2x} 2x`}
+            />
+            {/*Mobile*/}
             <img
                 srcSet={`${backgroundDesktop} 1x, ${backgroundDesktop2x} 2x`}
                 alt="image of a pizza used as background image to the hero section"
-                className="hidden lg:block w-full"
+                className="w-full"
             />
-        </>
+        </picture>
     );
 }
 
